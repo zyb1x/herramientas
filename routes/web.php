ec8a53dbc380981f62584d856a28b967a13afbae
Daniel <2124100021@soy.utj.edu.mx>
2026-02-15 22:04:17 -0600

    Primera carga

---
+++ routes/web.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+Route::view('/inicio', 'inicio.inicio')->name('inicio');
+Route::view('/login', '/login/login')->name('login');
