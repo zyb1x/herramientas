01db627fcae4a3f432ff23445a22bf8932d35140
zyb1x <dluis98@hotmail.com>
2026-02-16 22:48:04 -0600

    se agregan validaciones para el navbar de login

--- resources/views/login/login.blade.php
+++ resources/views/login/login.blade.php
@@ -5,14 +5,11 @@
 
 @section('contenido')
 
-    <section class="bg-gray-50 dark:bg-gray-900">
+    <section class="bg-gray-50 dark:white">
         <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-            <a href="#" class="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
-                <img class="w-8 h-8 mr-2" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg" alt="logo">
-                Flowbite
-            </a>
+            
             <div
-                class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+                class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-[#023047] dark:border-gray-700">
                 <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
                     <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
                         Inicia sesión
@@ -50,14 +47,14 @@
                                 </div>
                             </div>
                             <a href="#"
-                                class="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500">¿Olvidaste
+                                class="text-sm font-medium text-[#fb5607] hover:underline dark:text-primary-500">¿Olvidaste
                                 tu contraseña?</a>
                         </div>
                         <button type="submit"
-                            class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Sign
-                            in</button>
+                            class="w-full text-white bg-[#fb5607] hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300
+                            font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-[#fb5607] dark:hover:bg-primary-700 dark:focus:ring-primary-800">Iniciar sesión</button>
                         <a href="#"
-                            class="font-medium text-primary-600 hover:underline dark:text-primary-500">Registrate</a>
+                            class="font-medium text-[#fb5607] hover:underline dark:text-primary-500">Registrate</a>
 
                     </form>
                 </div>
